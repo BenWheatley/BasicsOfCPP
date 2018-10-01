d58a0d6103434926ddd261b667706c5ffa8706dd
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-10-01 11:13:29 +0200

    Initial Commit

---
+++ Learning CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning CPP
+//
+//  Created by Ben Wheatley on 2018/10/01.
+//  Copyright © 2018 Ben Wheatley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
