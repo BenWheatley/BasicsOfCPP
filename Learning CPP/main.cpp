51124044cbd87df33c93fac0f6c8880fb257f880
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-10-01 11:35:17 +0200

    Variables: before

--- Learning CPP/main.cpp
+++ Learning CPP/main.cpp
@@ -11,9 +11,9 @@
 // Following https://www.youtube.com/watch?v=vLnPwxZdW4Y tutorial
 
 int main(int argc, const char * argv[]) {
-	std::cout << "   /|\n";
-	std::cout << "  / |\n";
-	std::cout << " /  |\n";
-	std::cout << "/___|\n";
+	std::cout << "There was once a man named George\n";
+	std::cout << "He was 70 years old\n";
+	std::cout << "He liked being named George\n";
+	std::cout << "But he didn't like being 70\n";
 	return 0;
 }
