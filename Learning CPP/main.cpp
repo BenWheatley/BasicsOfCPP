1d97d9b694d31fffd642e491f1586a0e75c29867
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-10-02 22:27:35 +0200

    lesson mark: 2:45

--- Learning CPP/main.cpp
+++ Learning CPP/main.cpp
@@ -14,17 +14,20 @@
 
 using namespace std;
 
+int exponent(int base, int power) {
+	int result = 1;
+	for (int i=0; i<power; i++) {
+		result *= base;
+	}
+	return result;
+}
+
 int main(int argc, const char * argv[]) {
-	for (int i=0; i<10; ++i) {
-		cout << i << endl;
-	}
-	
-	cout << endl;
-	
-	int nums[] = {1,3,4,6,4,-2};
-	for (int i=0; i<sizeof(nums)/sizeof(int); ++i) {
-		cout << nums[i] << endl;
-	}
+	cout << exponent(10, 3) << endl;
+	cout << exponent(2, 10) << endl;
+	cout << exponent(16, 2) << endl;
+	cout << exponent(256, 3) << endl;
+	cout << exponent(2, 24) << endl;
 	
 	return 0;
 }
