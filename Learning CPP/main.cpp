029648841a4d277e31988032b43cc81e92e0a7de
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-10-02 22:08:12 +0200

    lesson mark: 2:13

--- Learning CPP/main.cpp
+++ Learning CPP/main.cpp
@@ -14,27 +14,13 @@
 
 using namespace std;
 
-string dayNumToName(int dayNum) {
-	switch (dayNum) {
-		case 0: return "Monday";
-		case 1: return "Tuesday";
-		case 2: return "Wednesday";
-		case 3: return "Thursday";
-		case 4: return "Friday";
-		case 5: return "Saturday";
-		case 6: return "Sunday";
-		default:
-			return "invalid day";
-	}
-}
-
 int main(int argc, const char * argv[]) {
-	int dayIn;
+	int i = 0;
 	
-	cout << "Day number: " << endl;
-	cin >> dayIn;
-	
-	cout << "Day name: " << dayNumToName(dayIn) << endl;
+	while (i<10) {
+		i++;
+		cout << i << endl;
+	}
 	
 	return 0;
 }
